b1122306d40e94cb14826e6d19187b901f0e0f8c
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-06-08 01:16:53 +0200

    Feature: Implement order creation and retrieval endpoints in payments service

--- payments/main.py
+++ payments/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests, time
 from fastapi import HTTPException
 
 app = FastAPI()
@@ -18,6 +21,45 @@ redis = get_redis_connection(
     port=6379,
     decode_responses=True
 )
+
+
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str  # pending, completed, refunded
+
+    class Meta:
+        database = redis
+
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks):  # id, quantity
+    body = await request.json()
+
+    req = requests.get('http://127.0.0.1:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    return order
+
 
 
 
